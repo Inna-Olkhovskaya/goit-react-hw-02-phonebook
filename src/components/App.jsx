762691e4e1568a7a5b67d0a2eab860feeaa2d777
Inna-Olkhovskaya <115504107+Inna-Olkhovskaya@users.noreply.github.com>
2023-05-17 23:42:32 +0300

    Created architecture of project
    1. hook up prop-types
    2. installed nanoid
    3. created components
    4. created main structure of APP.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './ContactFilter/ContactFiltr';
+import ContactList from './ContactList/ContactList';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  render() {
+    return (
+     <div>
+  <h1>Phonebook</h1>
+  <ContactForm />
+
+  <h2>Contacts</h2>
+  <Filter />
+  <ContactList />
+</div>
+    )
+  }
+}
+
+export default App;
