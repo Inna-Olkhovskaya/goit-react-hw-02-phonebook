4b544ac18b0a0f187fdd7db0fafa509d247a174e
Inna-Olkhovskaya <115504107+Inna-Olkhovskaya@users.noreply.github.com>
2023-05-18 21:46:19 +0300

    added markup to App
    1. Created structure of main component App;
    2. Reset styles added to index.css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,24 +2,75 @@ import { Component } from 'react';
 import ContactForm from './ContactForm/ContactForm';
 import Filter from './ContactFilter/ContactFiltr';
 import ContactList from './ContactList/ContactList';
+import css from 'index.css';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
 
-  render() {
-    return (
-     <div>
-  <h1>Phonebook</h1>
-  <ContactForm />
+  // Добавляет контакт (желательно сократить или вынести)
+  addContact = newContact => {
+    // Проверка на дубликат
+    const duplicateName = this.state.contacts.find(
+      contact => contact.name === newContact.name
+    );
 
-  <h2>Contacts</h2>
-  <Filter />
-  <ContactList />
-</div>
-    )
+    if (duplicateName) {
+      alert(`${newContact.name} is already on contacts`);
+      return;
+    }
+
+    this.setState(({ contacts }) => ({
+      contacts: [newContact, ...contacts],
+    }));
+  };
+
+  // Следит за полем фильтрации и пишет в стейт
+  changeFilter = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  // Фильтрует и возвращает результат фильтра
+  filterContacts = () => {
+    const { contacts, filter } = this.state;
+
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  // Удаляет контакт
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  render() {
+    const { filter } = this.state;
+    const filteredResults = this.filterContacts();
+
+    return (
+      <>
+        <h1 className={css.title}>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+        <h2 className={css.title}>Contacts</h2>
+        <Filter value={filter} onChange={this.changeFilter} />
+        <ContactList
+          contacts={filteredResults}
+          onDeleteContact={this.deleteContact}
+        />
+      </>
+    );
   }
 }
 
